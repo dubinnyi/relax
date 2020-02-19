8e8f49c728c5953b61c3adcc57e75b7946ea9a48
LevTG <tanysa97@mail.ru>
2020-02-19 11:53:18 +0300

    add all files

---
+++ tryFitToCompare.py
@@ -0,0 +1,29 @@
+import fitting as f
+import numpy as np
+
+from argparse import ArgumentParser
+from exp_model import CModel
+
+
+parser = ArgumentParser()
+parser.add_argument("-p", "--pairs", type=str, help="File with atom pairs definition")
+parser.add_argument("-f", "--func", type=str, help="File with acf or ccf")
+
+
+# [time, func, err^2]
+
+def main():
+    options = parser.parse_args()
+    fd = open(options.func, 'r')
+    data = fd.read().split('\n\n')
+    meanData = []
+    for arr in data:
+        with open('tmp.csv', 'w') as tmpfd:
+                tmpfd.write(arr)
+        meanData.append(np.loadtxt('tmp.csv', delimiter=','))
+
+    for arr in meanData:
+        fitModel = f.Fitting(arr[:, 1], arr[:, 2], arr[:, 0])
+        for nexp in range(2, 6):
+            fitModel.fmodel = CModel(nexp)
+            fitModel.fitting()
