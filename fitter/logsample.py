374f32094ec7dd6ed49e53c2dd61d9b9dcdae7bf
dubinnyi <OxyLuciFerin2@>
2020-05-05 16:49:42 +0300

    logsample with example

--- fitter/logsample.py
+++ fitter/logsample.py
@@ -1,7 +1,11 @@
 import numpy as np
 
+
 def calc_poly(p, t):
-    # Calculate polynomial
+    # Calculate polynomial with array of coefficients
+    # p - array of coefficients, shape (deg+1, K)
+    # t - time, the argument of polynomial
+    # result - array of shape (K,)
     n=p.shape[0] # degree of polynomial + 1
     poly=np.copy(p[0,:])
     for i in range(1,n):
@@ -9,31 +13,35 @@ def calc_poly(p, t):
         poly += p[i,:]
     return poly
 
-def logsample(dt, data, factor= 1.05, deg=2):
-    (nd,nt)=data.shape
+def logsample(data, **kwargs):
+    dt=     kwargs['dt']     if 'dt'     in kwargs else 1
+    deg=    kwargs['deg']    if 'deg'    in kwargs else 2
+    factor= kwargs['factor'] if 'factor' in kwargs else 1.05
+
+    (nd, nt)=data.shape
     datatr = np.transpose(data)
     tend = dt * nt
     time = np.linspace(0, tend, nt, endpoint=False)
-    nlog, t = 1, 1.00
+    nlog, t = 1, dt
     while t< tend:
         t= t * factor
         if int(t) > nlog:
             nlog+= 1
     logdata= np.zeros((nd,nlog))
     logtime= np.zeros((nlog))
-    l0, l1, ilog, t = 0, 1, 0, 1.00
+    l0, l1, ilog, t = 0, 1, 0, dt
     while t< tend*factor:
         t = t * factor
-        if int(t) <= l1:
+        l2 = int(t)
+        if l2 <= l1:
             continue
         #if int(t) > l0:
         #    break
-        print("(l0, l1) = ({},{})".format(l0,l1))
+        #print("(l0, l1) = ({},{})".format(l0,l1))
         subt= time[l0:l1]
-        nsubt= subt.shape[0]
         tmean= np.mean(subt)
         subdata= datatr[l0:l1,:]
-        deg_fit = min(nsubt-1,deg)
+        deg_fit = min(subt.shape[0]-1,deg)
         #print('subt.shape = {}'.format(subt.shape))
         #print('subdata.shape = {}'.format(subdata.shape))
         p = np.polyfit(subt, subdata, deg=deg_fit)
@@ -42,23 +50,42 @@ def logsample(dt, data, factor= 1.05, deg=2):
         #print("datamean.shape = {}".format(datamean.shape))
         logtime[ilog]= tmean
         logdata[:,ilog]= datamean
-        (l0, l1)= (l1, int(t))
+        (l0, l1)= (l1, l2)
         if l1> nt:
             l1= nt
         ilog+= 1
     return logtime, logdata
 
 def test_logsample():
-    nt= 100
+    import matplotlib.pyplot as plt
+    nt= 1000
     nfunc= 3
-    data=np.zeros((nfunc,nt))
-    t=np.linspace(0,nt-1,nt)
-    print(t)
-    for f in range(nfunc):
-        data[f,:]=np.copy(t)**f
-    print(data)
-    logdata=logsample(1.,data)
-    print(logdata)
+    data=np.zeros((nfunc+1,nt))
+    t=np.linspace(0.5,nt-1,nt)
+    #print(t)
+    for f in range(nfunc+1):
+        data[f,:]=np.exp(-np.copy(t)/(5*(f+1)))
+    #print(data)
+    factor=1.2
+    ltime, ldata= logsample(data, factor=factor)
+    #print(ldata)
+    print("size of logsamples data {}".format(ltime.shape[0]))
+
+    fig, ax =plt.subplots(1, 2, figsize=(20, 10))
+    ax[0].set_xscale('log')
+    ax[0].set_title("{} linear points".format(nt))
+    for f in range(nfunc+1):
+        ax[0].plot(t, data[f,:],'o-')
+    #ax[0].title('linear data')
+
+    ax[1].set_xscale('log')
+    ax[1].set_title("{} logsample points, factor={}".format(ltime.shape[0], factor))
+    for f in range(nfunc + 1):
+        ax[1].plot(ltime, ldata[f, :],'o-')
+    #ax[0].title('LOG samples data')
+    plt.show()
+    plt.close()
+
 
 if __name__ == '__main__':
     test_logsample()
