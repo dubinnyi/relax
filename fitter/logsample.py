5a93e4750a77e35c790243bb5c7ba6e95268ed99
dubinnyi <OxyLuciFerin2@>
2020-05-05 23:33:48 +0300

    logsample: two additional examples

--- fitter/logsample.py
+++ fitter/logsample.py
@@ -58,16 +58,24 @@ def logsample(data, **kwargs):
 
 def test_logsample():
     import matplotlib.pyplot as plt
-    nt= 1000
+    nt= 100000
     nfunc= 3
-    data=np.zeros((nfunc+1,nt))
-    t=np.linspace(0.5,nt-1,nt)
+    data =  np.zeros((nfunc + 1, nt))
+    noise = np.zeros((nfunc + 1, nt))
+    t=np.linspace(0,nt-1,nt)
+    sigma = 0.05
     #print(t)
     for f in range(nfunc+1):
-        data[f,:]=np.exp(-np.copy(t)/(5*(f+1)))
+        data[f,:]=np.exp(-np.copy(t)/(500*(f+1)))
+        noise[f,:]=np.random.normal(0,sigma,nt)
+    datanoise = data + noise
     #print(data)
-    factor=1.2
-    ltime, ldata= logsample(data, factor=factor)
+    factor=1.05
+    #################################################
+    #
+    ltime, ldata= logsample(datanoise, factor=factor)
+    #
+    #################################################
     #print(ldata)
     print("size of logsamples data {}".format(ltime.shape[0]))
 
@@ -75,15 +83,35 @@ def test_logsample():
     ax[0].set_xscale('log')
     ax[0].set_title("{} linear points".format(nt))
     for f in range(nfunc+1):
-        ax[0].plot(t, data[f,:],'o-')
+        ax[0].plot(t, datanoise[f,:],'-')
     #ax[0].title('linear data')
 
     ax[1].set_xscale('log')
     ax[1].set_title("{} logsample points, factor={}".format(ltime.shape[0], factor))
     for f in range(nfunc + 1):
-        ax[1].plot(ltime, ldata[f, :],'o-')
+        ax[1].plot(ltime, ldata[f, :],'-')
     #ax[0].title('LOG samples data')
     plt.show()
+
+    fig, ax = plt.subplots(figsize=(20, 10))
+    ax.set_xscale('log')
+    ax.set_title("{} linear points vs {} logsample ponts, noise={}".format(nt, ltime.shape[0], sigma))
+    ax.plot(t, data[0,:] + noise[0,:], '-')
+    ax.plot(ltime, ldata[0, :], 'o-')
+    plt.show()
+
+    ltime, ldata = logsample(data, factor=factor)
+
+    fig, ax = plt.subplots(figsize=(20, 10))
+    ax.set_xscale('log')
+    ax.set_title("exact exp(-t/500) vs {} logsample ponts".format(nt, ltime.shape[0]))
+    tlogt=10.**np.linspace(0,5,1000)
+    texp=np.exp(-np.copy(tlogt)/(500))
+    ax.plot(tlogt, texp, '-')
+    ax.plot(ltime, ldata[0, :], 'o-')
+    plt.show()
+
+
     plt.close()
 
 
